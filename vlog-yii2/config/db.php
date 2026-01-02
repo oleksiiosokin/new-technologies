88eda9057bdf0cb2d70876cf3e11ae2157d01369
oleksiiosokin <alexosokin17@gmail.com>
2026-01-02 18:57:13 +0200

    Init Yii2 project

---
+++ vlog-yii2/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_vlog',
+    'username' => 'root',
+    'password' => '00002222',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
